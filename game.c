ab63d3219d5c97c88f51a95f9ea22670d7943734
Francis Phan <hph44@uclive.ac.nz>
2021-10-16 03:30:47 +1300

    Add new file

---
+++ game.c
@@ -0,0 +1,36 @@
+/**
+ * Program: UCFK4 Snake
+ * Authors: Francis Phan, Richard Li
+ * Description: A reconstruction of the old fashioned Nokia Snake
+ *              game for the UC Fun Kit 4. This module covers the main
+ *              functionalities of the game. 
+*/
+
+
+// 1. Modules used in this module --------------------------------
+#include "system.h"
+#include "tinygl.h"
+#include "pacer.h"
+#include "paddle.h"
+#include "messages.h"
+#include "font3x5_1.h"
+#include "font.h"
+#include "game.h"
+
+
+// 2. Intialisation of tinygl ------------------------------------
+void tiny_init(void) 
+{
+    // Refer to the definitions of PACER_RATE, MESSAGE_RATE etc. in game.h
+    tinygl_init (PACER_RATE); 
+    tinygl_font_set (&font3x5_1);
+    tinygl_text_speed_set (MESSAGE_RATE);
+    tinygl_text_mode_set (TINYGL_TEXT_MODE_SCROLL);
+    tinygl_text_dir_set (TINYGL_TEXT_DIR_ROTATE);
+}
+
+// 3. Setting game initial state ---------------------------------
+void game_init(void)
+{
+    continue;
+}
