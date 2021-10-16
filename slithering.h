53ad9342b40c4f74da7ca9f328d510ec0190511c
Francis Phan <hph44@uclive.ac.nz>
2021-10-16 14:26:50 +1300

    Update slithering.h

--- slithering.h
+++ slithering.h
@@ -1,16 +1,24 @@
-/**  @file   slithering.h
-     @author Liyinqin Li
-     @date   16/10/21
+/**
+ * Program: UCFK4 Snake
+ * Module: Header file game.h for snake control module slitering.h
+ * Authors: Francis Phan, Richard Li
+ */
 
-*/
-
-#ifndef SNAKE_H
-#define SNAKE_H
+#ifndef SLITHERING_H
+#define SLITHERING_H
 
 #include <stdlib.h>
 #include "system.h"
 #include "tinygl.h"
-#include "ir_uart.h"
+
+#define START_SNAKE_MODE 0
+#define APPLE_MODE 1
+#define PACER_RATE 500
+#define LOOP_RATE 500
+#define MESSAGE_RATE 20
+#define BUTTON_RATE 300
+#define LED_ON 1
+#define LED_OFF 0
 
 /* Define the snake location and direction */
 #define SNAKE_START_X_POS 1
